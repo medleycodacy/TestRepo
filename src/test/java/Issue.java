f389d52f999aa69069774bcdcab14e7fa2a8966a
Bruno Medley <brunomedley@gmail.com>
2022-11-23 12:58:00 +0000

    add file with issue

---
+++ src/test/java/Issue.java
@@ -0,0 +1,69 @@
+import java.util.Optional;
+import org.codacy.framework.web.BaseTestWeb;
+import org.codacy.pages.web.Authentication;
+import org.codacy.pages.web.Homepage;
+import org.codacy.pages.web.LoginMethod;
+import org.codacy.pages.web.ProjectWebPage;
+import org.testng.annotations.Test;
+
+
+@Test(alwaysRun = true)
+public class issue extends BaseTestWeb {
+    
+
+    public void tc03setUp() {
+        //todo
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET);
+    }
+
+    public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+
+    public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+
+    public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+
+    public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+
+    public void tc03setUp() {
+        Authentication authentication = new Authentication(driver, testEnvironment);
+        Homepage homepage = new Homepage(driver, testEnvironment);
+
+        homepage.landingPage();
+        authentication.login(LoginMethod.BITBUCKET, testEnvironment.email(), testEnvironment.password(), Optional.empty());
+    }
+
+    @Test(dependsOnMethods = "tc03setUp")
+    public void tc03LoginWithBitBucket() throws Throwable {
+        ProjectWebPage projectPage = new ProjectWebPage(driver, testEnvironment);
+
+        projectPage.validateCommitTable();
+    }
+}
